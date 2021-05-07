b0d9e6fbadc11127ee41164682832bd7c9c20c7b
vboomi <vboomi@users.noreply.github.com>
2021-05-07 10:15:26 -0500

    updated tags

--- data/tag_categories.js
+++ data/tag_categories.js
@@ -2,6 +2,13 @@ define({
 	tagCategories: {
 		"type": {
 			"description": "type of the paper"
-		}
+		},
+		"mask": {},
+		"sensor": {},
+		"psf": {},
+		"application": {},
+		"source": {},
+		"algorithm": {},
+		"thickness": {}
 	}
 });
